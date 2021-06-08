41b4f37dc0e598a987c34278cc8e437ec1086394
Jasmeen Kaur <76175793+jasmeenkaur7202@users.noreply.github.com>
2021-06-08 22:02:07 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Do Vaari Jatt",
+    artist: "Jordan Sandhu",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Kya Baat Ay",
+    artist: "Harrdy Sandhu",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Info",
+    artist: "Jordhan Sandhu",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Clash",
+    artist: "Diljit Dosanjh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Sira E Hou",
+    artist: "Speeds Records",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Khabbi Seat",
+    artist: "Ammy Virk",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "8 Parche",
+    artist: "Baani Sandhu Ft Gur Sidhu",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Shadgi",
+    artist: "Parmish Verma",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Jhanjar",
+    artist: "Karan Aujla",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Pani di Gal",
+    artist: "Maninder Buttar",
+    img: "music-10",
+    src: "music-10"
+  },
+];    
